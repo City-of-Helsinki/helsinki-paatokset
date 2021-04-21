2225e3a551b191723982e3103e5cc98ad4f8122a
Juho Lehmonen <juho.lehmonen@siili.com>
2021-04-21 12:37:00 +0300

    PP-86: PP-86: Remove unused styles, fix footer menu and JS error.

--- public/themes/custom/helfi_paatokset/src/js/common.js
+++ public/themes/custom/helfi_paatokset/src/js/common.js
@@ -1,70 +1,9 @@
-(($, Drupal, drupalSettings) => {
-  Drupal.behaviors.asuAdminCommon = {
+// eslint-disable-next-line no-unused-vars
+((Drupal, drupalSettings) => {
+  Drupal.behaviors.themeCommon = {
     attach: function attach() {
       // Code here.
-    }
+    },
   };
-
-  Drupal.behaviors.languageSwitcher = {
-    attach: function attach(context) {
-      const languageSwitcherToggleButton = $(
-        ".language-switcher__button",
-        context
-      );
-      const languageSwitcherWrapper = $(
-        ".language-switcher__dropdown",
-        context
-      );
-
-      const outsideClickListener = function outsideClickListener(event) {
-        const target = $(event.target);
-
-        if (
-          !target.closest(".language-switcher__dropdown").length &&
-          $(".language-switcher__dropdown").is(":visible")
-        ) {
-          // eslint-disable-next-line no-use-before-define
-          handleInteraction(event);
-          // eslint-disable-next-line no-use-before-define
-          removeClickListener();
-        }
-      };
-
-      const removeClickListener = function removeClickListener() {
-        document.removeEventListener("click", outsideClickListener);
-      };
-
-      function handleInteraction(e) {
-        e.stopImmediatePropagation();
-
-        if (languageSwitcherWrapper.hasClass("is-active")) {
-          languageSwitcherWrapper
-            .removeClass("is-active")
-            .attr("aria-hidden", "true");
-          languageSwitcherToggleButton.attr("aria-expanded", "false");
-        } else {
-          languageSwitcherWrapper
-            .addClass("is-active")
-            .attr("aria-hidden", "false");
-          languageSwitcherToggleButton.attr("aria-expanded", "true");
-          document.addEventListener("click", outsideClickListener);
-        }
-      }
-
-      languageSwitcherToggleButton.on({
-        click: function touchstartclick(e) {
-          handleInteraction(e);
-        },
-        keydown: function keydown(e) {
-          if (e.which === 27) {
-            languageSwitcherWrapper
-              .removeClass("is-active")
-              .attr("aria-hidden", "true");
-            languageSwitcherToggleButton.attr("aria-expanded", "false");
-            removeClickListener();
-          }
-        }
-      });
-    }
-  };
-})(jQuery, Drupal, drupalSettings);
+  // eslint-disable-next-line no-undef
+})(Drupal, drupalSettings);
