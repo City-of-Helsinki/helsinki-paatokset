1c4a1fc2f6fe2dcbc81614d6b4a0b26e7c55766c
Jere Ljungberg <jere@mysteerio.fi>
2021-08-04 11:27:42 +0300

    PP-188: Helsinki Kanava -integration
    - Added new migration paatokset_meeting_videos
    - Added latest recording section to policymaker template
    - Added new setting to identify which policymaker node is city council

---
+++ public/modules/custom/paatokset_helsinki_kanava/src/Entity/MeetingVideo.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\paatokset_helsinki_kanava\Entity;
+
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\RevisionLogEntityTrait;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Entity\ContentEntityBase;
+
+/**
+ * Defines meeting_video entity class.
+ *
+ * @ContentEntityType(
+ *   id = "meeting_video",
+ *   label = @Translation("Meeting video"),
+ *   handlers = {
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\Core\Entity\EntityListBuilder",
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *     "access" = "Drupal\helfi_api_base\Entity\Access\RemoteEntityAccess",
+ *     "form" = {
+ *       "default" = "Drupal\Core\Entity\ContentEntityForm",
+ *       "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm"
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\helfi_api_base\Entity\Routing\EntityRouteProvider",
+ *     }
+ *   },
+ *   base_table = "paatokset_meeting_video",
+ *   data_table = "paatokset_meeting_video_field_data",
+ *   revision_table = "paatokset_meeting_video_revision",
+ *   revision_data_table = "paatokset_meeting_video_field_revision",
+ *   show_revision_ui = TRUE,
+ *   translatable = TRUE,
+ *   admin_permission = "administer remote entities",
+ *   entity_keys = {
+ *    "id" = "id",
+ *    "revision" = "revision_id",
+ *    "langcode" = "langcode",
+ *    "uuid" = "uuid",
+ *    "uid" = "uid",
+ *    "label" = "label"
+ *   },
+ *   revision_metadata_keys = {
+ *    "revision_created" = "revision_timestamp",
+ *    "revision_user" = "revision_user",
+ *    "revision_log_message" = "revision_log"
+ *   },
+ *   links = {
+ *     "canonical" = "/meeting-video/{meeting_video}",
+ *     "edit-form" = "/admin/content/integrations/meeting-video/{meeting_video}/edit",
+ *     "delete-form" = "/admin/content/integrations/meeting-video/{meeting_video}/delete",
+ *     "collection" = "/admin/content/integrations/meeting-video"
+ *   },
+ *   field_ui_base_route = "meeting_video.settings"
+ * )
+ */
+final class MeetingVideo extends ContentEntityBase {
+  use RevisionLogEntityTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['name'] = BaseFieldDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('name'))
+      ->setTranslatable(TRUE)
+      ->setRevisionable(TRUE)
+      ->setDefaultValue('')
+      ->setCardinality(1)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setSettings([
+        'max_length' => 255,
+        'text_processing' => 0,
+      ]);
+
+    $fields['embed_url'] = BaseFieldDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('embed_url'))
+      ->setTranslatable(TRUE)
+      ->setRevisionable(TRUE)
+      ->setDefaultValue('')
+      ->setCardinality(1)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setSettings([
+        'max_length' => 255,
+        'text_processing' => 0,
+      ]);
+
+    return $fields;
+  }
+
+}
---
+++ public/modules/custom/paatokset_helsinki_kanava/src/Form/HelsinkiKanavaForm.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\paatokset_helsinki_kanava\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+
+/**
+ * Form for Helsinki Kanava settings.
+ */
+class HelsinkiKanavaForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'paatokset_helsinki_kanava.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'paatokset_helsinki_kanava_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('paatokset_helsinki_kanava.settings');
+
+    $form['city_council_node'] = [
+      '#type' => 'entity_autocomplete',
+      '#target_type' => 'node',
+      '#selection_settings' => [
+        'target_bundles' => ['policymaker'],
+      ],
+      '#title' => $this->t('City council'),
+      '#description' => $this->t('Select the city council node. Helsinki kanava videos are displayed only on this page.'),
+      '#default_value' => Node::load($config->get('city_council_node')),
+    ];
+
+    $form['all_recordings_link'] = [
+      '#type' => 'url',
+      '#default_value' => $config->get('all_recordings_link'),
+      '#title' => t('All recordings link'),
+      '#description' => 'Link to listing on helsinkikanava.fi containing all recorded meetings',
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('paatokset_helsinki_kanava.settings')
+      ->set('city_council_node', $form_state->getValue('city_council_node'))
+      ->set('all_recordings_link', $form_state->getValue('all_recordings_link'))
+      ->save();
+  }
+
+}
