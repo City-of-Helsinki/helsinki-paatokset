dfa9152ed95cae7a413145b3e931f276f61c2b35
Anna Lonka <anna.lonka@siilim210404.local>
2021-10-26 15:50:57 +0300

    PP-148: PP-148: removed unnecessary code

--- public/modules/custom/paatokset_ahjo/src/Plugin/Block/CalendarBlock.php
+++ public/modules/custom/paatokset_ahjo/src/Plugin/Block/CalendarBlock.php
@@ -19,14 +19,12 @@ class CalendarBlock extends BlockBase {
    *
    * @var Drupal\paatokset_ahjo\Service\PolicymakerService
    */
-  private $policymakerService;
 
   /**
    * Class constructor.
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->policymakerService = \Drupal::service('Drupal\paatokset_ahjo\Service\PolicymakerService');
   }
 
   /**
@@ -36,35 +34,10 @@ class CalendarBlock extends BlockBase {
     return [
       '#cache' => ['contexts' => ['url.path', 'url.query_args']],
       '#title' => t('Calendar'),
-      '#meetings' => ['joku', 'seuraava', 'testitestinen'],
       '#attributes' => [
         'class' => ['policymaker-calendar', 'container'],
       ],
     ];
-  }
-
-
-  /**
-   * Builds render arrays of meetings and return them.
-   */
-  private function getMeetings() {
-    $policymaker = $this->policymakerService->getPolicymaker();
-
-    if (!$policymaker) {
-      return;
-    }
-    
-
-    $renderableEntities = [];
-    $entities = $policymaker->get('field_contacts')->referencedEntities();
-    foreach ($entities as $entity) {
-      $view_builder = \Drupal::entityTypeManager()->getViewBuilder('tpr_unit');
-      $build = $view_builder->view($entity, 'contact_card');
-
-      $renderableEntities[] = $build;
-    }
-
-    return $renderableEntities;
   }
 
   /**
