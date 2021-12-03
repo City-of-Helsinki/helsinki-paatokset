e3bc9be6604f7dac93b99c4c471be264a2b6e167
Jere Ljungberg <jere@mysteerio.fi>
2021-12-03 10:10:02 +0200

    PP-253: Embed search application

---
+++ public/modules/custom/paatokset_search/src/Controller/SearchController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\paatokset_search\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Controller.
+ */
+class SearchController extends ControllerBase {
+
+  /**
+   * Return markup for search page.
+   */
+  public function decisions() {
+    $build = [
+      '#markup' => '<div id="paatokset_search"></div>',
+      '#attached' => [
+        'library' => [
+          'paatokset_search/paatokset-search',
+        ],
+      ],
+    ];
+
+    return $build;
+  }
+
+}
