9f25a2cfc7e06a876da2b1f6fd32d8f3b94652b0
Anna Lonka <anna.lonka@siilim210404.local>
2021-12-15 09:16:31 +0200

    PP-133: PP-133: created meetings calendar block and styled it

---
+++ public/modules/custom/paatokset_ahjo/src/Plugin/Block/MeetingsCalendarBlock.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\paatokset_ahjo\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides Calendar Block.
+ *
+ * @Block(
+ *    id = "meetings_calendar",
+ *    admin_label = @Translation("Paatokset meetings calendar"),
+ *    category = @Translation("Paatokset custom blocks")
+ * )
+ */
+class MeetingsCalendarBlock extends BlockBase {
+
+  /**
+   * Build the attributes.
+   */
+  public function build() {
+    return [
+      '#cache' => ['contexts' => ['url.path', 'url.query_args']],
+      'label' => t('Upcoming meetings'),
+      '#attributes' => [
+        'class' => ['meetings-calendar'],
+      ],
+    ];
+  }
+
+  /**
+   * Get cache contexts.
+   */
+  public function getCacheContexts() {
+    return ['url.path', 'url.query_args'];
+  }
+
+}
