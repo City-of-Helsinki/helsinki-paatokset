db25906e9dccbe1a94a65e939fd95edca473d262
Anna Lonka <anna.lonka@siilim210404.local>
2021-12-21 15:09:41 +0200

    PP-134: PP-134: continued with members block

--- public/modules/custom/paatokset_ahjo/js/policymaker_members.js
+++ public/modules/custom/paatokset_ahjo/js/policymaker_members.js
@@ -11,200 +11,143 @@
     attach() {
       const markup = `
       <div class="policymaker-members container">
-        testi testinen
+        <div class="policymaker-members__filters">
+          <div class="search-wrapper">
+            <label>Hae valtuutettua</label>
+            <input type="text" v-model="search" placeholder="Syötä valtuutetun nimi"/>
+          </div>
+          <div v-for="(filter, key) in filters" class="form-item">
+            <div class="form-item__dropdown">
+              <label>{{ filter.label }}</label>
+              <select class="form-item__select" id="select" v-model="active_filters[key]">
+                <option :value="Object.keys(filter)[0]">{{Object.keys(filter)[0]}}</option>
+                <option v-for="value in Object.values(filter)[0]" v-bind:value="value">{{ value }}</option>
+              </select>
+            </div>
+          </div>
+          <div class="filters-checkboxes">
+              <li v-for="checkbox in checkboxes" class="form-item--checkbox__item">
+                <label :for="Object.keys(checkbox)[0]">
+                  <input :checked="active_checkboxes.includes(Object.keys(checkbox)[0])" :id="Object.keys(checkbox)[0]" name="checkbox" type="checkbox" @click="addToActiveCheckbox(Object.keys(checkbox)[0])">
+                  <span>{{ Object.values(checkbox)[0] }}</span>
+                </label>
+              </li>
+            </div>
+        </div>
+        <div class="policymaker-members__list">
+          <div v-for="member in filteredMembers" class="member-row">
+            <div class="member-info">
+              <span class="member-name">{{ member.name }}</span>
+              <div>
+                <span class="member-role">{{ member.role }}</span>
+                <span class="member-party"> {{ member.party }}</span>
+              </div>
+              <span class="member-email">{{ processEmail(member.email) }}</span>
+            </div>
+            <div class="member-image"></div>
+          </div>
+        </div>
       </div>
       `
-      
-      /* URL to get all meetings */
-      const yearAgo = dayjs().subtract(1, "year").format("YYYY-MM-DD");
-      const dataURL = window.location.origin + '/fi/ahjo_api/meetings?from=' + yearAgo;
 
       new Vue({
         el: '#policymaker-members-vue',
         template: markup,
         data: {
-          meetings: [],
-          daysWithMeetings:[],
+          members: [
+            {
+              'name': 'Aki Hyödynmaa',
+              'role': 'Varajäsen',
+              'links': [],
+              'deputyOf': 'Salla Korhonen',
+              'email' : 'etunimi.sukunimi@email.com',
+              'party' : 'Vasemmisto'
+            },
+            {
+              'name': 'Antti Vuorela',
+              'role': 'Varapuheenjohtaja',
+              'links': [],
+              'deputyOf': null,
+              'email' : 'etunimi.sukunimi@email.com',
+              'party' : 'Kokoomus'
+            },
+            {
+              'name': 'Mikko Mallikas',
+              'role': 'Varajäsen',
+              'links': [],
+              'deputyOf': null,
+              'email' : 'etunimi.sukunimi@email.com',
+              'party' : 'Kokoomus'
+            },
+            {
+              'name': 'Testi Testinen',
+              'role': 'Joku rooli',
+              'links': [],
+              'deputyOf': null,
+              'email' : 'etunimi.sukunimi@email.com',
+              'party' : 'Kokoomus'
+            },
+            {
+              'name': 'Joku Jokunen',
+              'role': 'Varajäsen',
+              'links': [],
+              'deputyOf': null,
+              'email' : 'etunimi.sukunimi@email.com',
+              'party' : 'Kokoomus'
+            }
+          ],
           isReady: false,
-          selectedDate: dayjs(),
-          currentDate: dayjs().format("YYYY-MM-DD"),
+          search: '',
+          filters: {
+            party: { 'Kaikki': ['Kokoomus', 'Vasemmisto'], label : 'Rajaa puolueen mukaan' },
+            order: { 'A-Ö, nimen mukaan': ['A-Ö, puolueen mukaan'], label: 'Lajittelu'}
+          },
+          active_filters: {
+            party: 'Kaikki',
+            order: 'A-Ö, nimen mukaan'
+          },
+          checkboxes: {
+            deputy_member: { deputy_member: 'Näytä varajäsenet'}
+          },
+          active_checkboxes: [],
         },
         methods: {
-          getJson() {
-            const self = this;
-            $.getJSON(dataURL, function(data) {
-              self.meetings = data.data;
-            })
-            .done(function( json ) {
-              self.isReady = true;
-              let temp = self.days;
-              temp.forEach(function (day) {
-                var date = day.date;
-                if(self.meetings[date]) {
-                  day.meetings = self.meetings[date]
-                }
-              });
-              
-              self.daysWithMeetings = temp;
-            })
-          },
-          selectPrevious() {
-            let newSelectedDate = dayjs(this.selectedDate).subtract(1, "month");
-            this.selectedDate = newSelectedDate;
-            const self = this;
-            let temp = this.days;
-            temp.forEach(function (day) {
-              var date = day.date;
-              if(self.meetings[date]) {
-                day.meetings = self.meetings[date]
-              }
-            });
-
-            self.daysWithMeetings = temp;
-          },
-          selectNext() {
-            let newSelectedDate = dayjs(this.selectedDate).add(1, "month");
-            this.selectedDate = newSelectedDate;
-
-            const self = this;
-            let temp = this.days;
-            temp.forEach(function (day) {
-              var date = day.date;
-              if(self.meetings[date]) {
-                day.meetings = self.meetings[date]
-              }
-            });
-
-            self.daysWithMeetings = temp;
-          },
-          formatDay(date) {
-            return dayjs(date).format("D.M");
-          },
-          getDay(date) {
-            if(dayjs(date).day() === 1) {
-              return Drupal.t('Maanantai');
-            }
-            if(dayjs(date).day() === 2) {
-              return Drupal.t('Tiistai');
-            }
-            if(dayjs(date).day() === 3) {
-              return Drupal.t('Keskiviikko');
-            }
-            if(dayjs(date).day() === 4) {
-              return Drupal.t('Torstai');
-            }
-            if(dayjs(date).day() === 5) {
-              return Drupal.t('Perjantai');
-            }
-            if(dayjs(date).day() === 6) {
-              return Drupal.t('Lauantai');
-            }
-            if(dayjs(date).day() === 0) {
-              return Drupal.t('Sunnuntai');
-            }
-          },
-          isToday(date) {
-            return dayjs(date).format("YYYY-MM-DD") === this.currentDate;
-          },
-          isPast(date) {
-            return dayjs(date).format("YYYY-MM-DD") < this.currentDate;
-          },
-          getWeekday(date) {
-            return dayjs(date).weekday();
-          },
-        },
-        computed: {
-          selectedMonth() {
-            return Drupal.t(this.selectedDate.format("MMMM"), {}, {context: "Long month name"});
-          },
-          weekdays() {
-            return weekdaysData;
-          },
-          today() {
-            return dayjs().format("YYYY-MM-DD");
-          },
-          month() {
-            return Number(this.selectedDate.format("M"));
-          },
-          year() {
-            return Number(this.selectedDate.format("YYYY"));
-          },
-          numberOfDaysInMonth() {
-            return dayjs(this.selectedDate).daysInMonth();
-          },
-          openMotions(){
-            return Drupal.t('Avaa esityslista')
-          },
-          openMinutes() {
-            return Drupal.t('Katso pöytäkirja')
-          },
-          openDecisions() {
-            return Drupal.t('Katso päätöstiedote')
-          },
-          days() {
-            let allDays = [];
-            if(this.previousMonthDays) {
-              allDays = [
-                ...this.previousMonthDays,
-                ...this.currentMonthDays,
-                ...this.nextMonthDays
-              ];
+          addToActiveCheckbox(value) {
+            let temp_filters = this.active_checkboxes;
+            if(!temp_filters.includes(value)) {
+              temp_filters.push(value)
             } else {
-              allDays = [
-                ...this.currentMonthDays,
-                ...this.nextMonthDays
-              ];
+              const index = temp_filters.indexOf(value);
+              if (index !== -1) {
+                temp_filters.splice(index, 1);
+              }
             }
-
-            let temp = allDays
-            temp = temp.filter(result => dayjs(result.date).day() !== 6 && dayjs(result.date).day() !== 0);
-            return temp;
+            this.active_checkboxes = temp_filters;
           },
-          currentMonthDays() {
-            return [...Array(this.numberOfDaysInMonth)].map((day, index) => {
-              return {
-                date: dayjs(`${this.year}-${this.month}-${index + 1}`).format("YYYY-MM-DD"),
-                isCurrentMonth: true,
-                meetings: []
-              };
-            });
-          },
-          previousMonthDays() {
-            const firstDayOfTheMonthWeekday = dayjs(this.currentMonthDays[0].date).day();
-            const previousMonth = dayjs(`${this.year}-${this.month}-01`).subtract(1, "month");
-
-            const visibleNumberOfDaysFromPreviousMonth = firstDayOfTheMonthWeekday ? firstDayOfTheMonthWeekday - 1 : 6;
-        
-            const previousMonthLastMondayDayOfMonth = dayjs(this.currentMonthDays[0].date).subtract(visibleNumberOfDaysFromPreviousMonth, "day").date();
-            
-            //Doesn't add previous days if the first day of the month is saturday or sunday
-            if(firstDayOfTheMonthWeekday !== 6 && firstDayOfTheMonthWeekday !== 0) {
-              return [...Array(visibleNumberOfDaysFromPreviousMonth)].map((day, index) => {
-                return {
-                  date: dayjs(`${previousMonth.year()}-${previousMonth.month() + 1}-${previousMonthLastMondayDayOfMonth + index}`).format("YYYY-MM-DD"),
-                  isCurrentMonth: false,
-                  meetings: []
-                };
-              });
-            }
-          },
-          nextMonthDays() {
-            const lastDayOfTheMonthWeekday = dayjs(`${this.year}-${this.month}-${this.currentMonthDays.length}`).day();
-            const nextMonth = dayjs(`${this.year}-${this.month}-01`).add(1, "month");
-            const visibleNumberOfDaysFromNextMonth = lastDayOfTheMonthWeekday ? 7 - lastDayOfTheMonthWeekday : lastDayOfTheMonthWeekday;
-        
-            return [...Array(visibleNumberOfDaysFromNextMonth)].map((day, index) => {
-              return {
-                date: dayjs(`${nextMonth.year()}-${nextMonth.month() + 1}-${index + 1}`).format("YYYY-MM-DD"),
-                isCurrentMonth: false,
-                meetings: []
-              };
-            });
+          processEmail(email) {
+            return email.replace("@", " (at) ")
           }
         },
-        mounted() {
-          this.getJson();
-        },
+        computed: {
+          filteredMembers() {
+            let temp_results = this.members;
+            temp_results = temp_results.filter(result => {
+              return result.name.toLowerCase().includes(this.search.toLowerCase())
+            })
+
+            for (var i = 0; i < this.active_checkboxes.length; i++) {
+              if(this.active_checkboxes[i] === 'deputy_member') {
+                temp_results = temp_results.filter(result => result.role === 'Varajäsen')
+              }
+            }
+
+            if(this.active_filters.party !== 'Kaikki') {
+              temp_results = temp_results.filter(result => result.party === this.active_filters.party)
+            }
+
+            return temp_results;
+          }
+        }
       });
     }
   }
---
+++ public/modules/custom/paatokset_ahjo/src/Plugin/Block/MembersBlock.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\paatokset_ahjo\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides Calendar Block.
+ *
+ * @Block(
+ *    id = "policymaker_members",
+ *    admin_label = @Translation("Paatokset policymaker members"),
+ *    category = @Translation("Paatokset custom blocks")
+ * )
+ */
+class MembersBlock extends BlockBase {
+
+  /**
+   * Build the attributes.
+   */
+  public function build() {
+    return [
+      '#cache' => ['contexts' => ['url.path', 'url.query_args']],
+      '#title' => t('Members'),
+      '#attributes' => [
+        'class' => ['policymaker-members'],
+      ],
+    ];
+  }
+
+  /**
+   * Get cache contexts.
+   */
+  public function getCacheContexts() {
+    return ['url.path', 'url.query_args'];
+  }
+
+}
