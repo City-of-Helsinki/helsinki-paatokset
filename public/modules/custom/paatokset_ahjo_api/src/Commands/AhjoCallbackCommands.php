55d81ae4b69992bb3fb38a830d5029c307dee9a9
Juho Lehmonen <juho.lehmonen@siili.com>
2022-02-18 10:59:32 +0200

    PP-221: Implement Ahjo callback queue commands and queueworker.

---
+++ public/modules/custom/paatokset_ahjo_api/src/Commands/AhjoCallbackCommands.php
@@ -0,0 +1,235 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\paatokset_ahjo_api\Commands;
+
+use Drush\Commands\DrushCommands;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Queue\QueueFactory;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\paatokset_ahjo_proxy\AhjoProxy;
+use Symfony\Component\Console\Helper\Table;
+
+/**
+ * Ahjo Callback drush commands.
+ *
+ * @package Drupal\paatokset_ahjo_api\Commands
+ */
+class AhjoCallbackCommands extends DrushCommands {
+
+  private const QUEUE_NAME = 'ahjo_api_subscriber_queue';
+
+  /**
+   * Ahjo callback queue.
+   *
+   * @var \Drupal\Core\Queue\QueueInterface
+   */
+  protected $queue;
+
+  /**
+   * Ahjo proxy service.
+   *
+   * @var \Drupal\paatokset_ahjo_proxy\AhjoProxy
+   */
+  protected $ahjoProxy;
+
+  /**
+   * Queue Factory.
+   *
+   * @var \Drupal\Core\Queue\QueueFactory
+   */
+  protected $queueFactory;
+
+  /**
+   * The logger.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected $logger;
+
+  /**
+   * The database.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * Constructor for Ahjo Callback commands.
+   *
+   * @param \Drupal\Core\Queue\QueueFactory $queue_factory
+   *   Queue service.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   *   Logger service.
+   * @param \Drupal\Core\Database\Connection $database
+   *   The database connection.
+   * @param \Drupal\paatokset_ahjo_proxy\AhjoProxy $ahjo_proxy
+   *   Ahjo Proxy service.
+   */
+  public function __construct(QueueFactory $queue_factory, LoggerChannelFactoryInterface $logger_factory, Connection $database, AhjoProxy $ahjo_proxy) {
+    $this->queueFactory = $queue_factory;
+    $this->queue = $this->queueFactory->get(self::QUEUE_NAME);
+    $this->logger = $logger_factory->get('ahjo_api_subscriber');
+    $this->database = $database;
+    $this->ahjoProxy = $ahjo_proxy;
+  }
+
+  /**
+   * List queue contents.
+   *
+   * @param string|null $name
+   *   Queue name. NULL for all.
+   *
+   * @command ahjo-callback:list-queue
+   *
+   * @usage ahjo-callback:list-queue meetings
+   *   Lists queue contents for "meetings" endpoint.
+   *
+   * @aliases ac:l
+   */
+  public function listCallbackQueue(?string $name = NULL): void {
+    if ($name) {
+      $this->output()->writeln('Queue: ' . $name);
+    }
+    else {
+      $this->output()->writeln('All queues.');
+    }
+
+    $table = new Table($this->output());
+    $table->setHeaders([
+      'Queue', 'ID', 'Time', 'Operation', 'Entity',
+    ]);
+
+    $items = [];
+    while ($item = $this->queue->claimItem()) {
+      $items[] = $item;
+
+      if ($name !== NULL && $item->data['id'] !== $name) {
+        continue;
+      }
+
+      if (isset($item->data['content']->updatetype)) {
+        $operation = $item->data['content']->updatetype;
+      }
+      else {
+        $operation = NULL;
+      }
+
+      if (isset($item->data['content']->id)) {
+        $entity = $item->data['content']->id;
+      }
+      else {
+        $entity = NULL;
+      }
+
+      $table->addRow([
+        $item->data['id'],
+        $item->item_id,
+        date('Y-m-d H:i:s', (int) $item->created),
+        $operation,
+        $entity,
+      ]);
+    }
+
+    // Release claimed items.
+    foreach ($items as $item) {
+      $this->queue->releaseItem(($item));
+    }
+
+    $table->render();
+  }
+
+  /**
+   * Deletes single item from queue.
+   *
+   * @param string $id
+   *   Item ID.
+   *
+   * @command ahjo-callback:delete-item
+   *
+   * @usage ahjo-callback:delete-item 1234
+   *   Delete item with ID 1234.
+   *
+   * @aliases ac:di
+   */
+  public function deleteItem(string $id): void {
+    if (!$item = $this->loadItem($id)) {
+      $this->logger()->error('Unable to load item with id: ' . $id);
+      return;
+    }
+
+    try {
+      $this->queue->deleteItem($item);
+      $this->logger()->success('Removed item ' . $id .' from the queue.');
+    }
+    catch (\Exception $e) {
+      $this->logger()->error('Error removing item from queue: ' . $e->getMessage());
+    }
+  }
+
+  /**
+   * Clear queue contents.
+   *
+   * @param string|null $name
+   *   Queue name. NULL for all.
+   *
+   * @command ahjo-callback:clear-queue
+   *
+   * @usage ahjo-callback:clear-queue meetings
+   *   Clear queue contents for "meetings" endpoint.
+   *
+   * @aliases ac:clear
+   */
+  public function clearQueue(?string $name = NULL): void {
+    if ($name) {
+      $this->output()->writeln('Clearing queue: ' . $name);
+    }
+    else {
+      $this->output()->writeln('Clearing all queues.');
+    }
+
+    if (!$this->io()->confirm('Are you sure?')) {
+      return;
+    }
+
+    $count = 0;
+    $items = [];
+    while ($item = $this->queue->claimItem()) {
+      if ($name !== NULL && $item->data['id'] !== $name) {
+        $items[] = $item;
+        continue;
+      }
+
+      $this->queue->deleteItem($item);
+      $count++;
+    }
+
+    // Release claimed items that weren't deleted.
+    foreach ($items as $item) {
+      $this->queue->releaseItem(($item));
+    }
+    $this->output()->writeln('Deleted ' . $count . ' items.');
+  }
+
+  /**
+   * Load a specified Ahjo API queue item from the database.
+   *
+   * @param string $item_id
+   *   The item id to load.
+   *
+   * @return mixed
+   *   Result of the database query loading the queue item.
+   */
+  private function loadItem(string $item_id) {
+    // Load the specified queue item from the queue table.
+    $query = $this->database->select('queue', 'q')
+      ->fields('q', ['item_id', 'name', 'data', 'expire', 'created'])
+      ->condition('q.item_id', $item_id)
+      ->condition('q.name', self::QUEUE_NAME)
+      // Item id should be unique.
+      ->range(0, 1);
+
+    return $query->execute()->fetchObject();
+  }
+}
---
+++ public/modules/custom/paatokset_ahjo_api/src/Plugin/QueueWorker/AhjoCallbackQueueWorker.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\paatokset_ahjo_api\Plugin\QueueWorker;
+
+use Drupal\Core\Logger\LoggerChannelInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Queue\QueueWorkerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Queue\SuspendQueueException;
+
+/**
+ * Processes cron queue.
+ *
+ * @QueueWorker(
+ *   id = "ahjo_api_subscriber_queue",
+ *   title = @Translation("Ahjo Callback Queue Worker"),
+ * )
+ */
+class AhjoCallbackQueueWorker extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The logger.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected LoggerChannelInterface $logger;
+
+  /**
+   * Ahjo proxy service.
+   *
+   * @var \Drupal\paatokset_ahjo_proxy\AhjoProxy
+   */
+  protected $ahjoProxy;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = new static($configuration, $plugin_id, $plugin_definition);
+    $instance->ahjoProxy = $container->get('paatokset_ahjo_proxy');
+    $instance->logger = $container->get('logger.factory')->get('ahjo_api_subscriber_queue');
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processItem($item) {
+    if (isset($item['content']->updatetype)) {
+      $operation = $item['content']->updatetype;
+    }
+    else {
+      $operation = NULL;
+    }
+
+    if (isset($item['content']->id)) {
+      $entity = $item['content']->id;
+    }
+    else {
+      $entity = NULL;
+    }
+
+    if (!$entity || !$operation) {
+      $this->logger->info('Empty callback from @queue queue, deleting.', [
+        '@queue' => $item['id'],
+      ]);
+      return;
+    }
+
+    if (!$this->ahjoProxy->isOperational()) {
+      $this->logger->error('Ahjo Proxy is not operational, suspending.');
+      throw new SuspendQueueException('Ahjo Proxy is not operational, suspending.');
+    }
+
+    $status = $this->ahjoProxy->migrateSingleEntity($item['id'], $entity);
+
+    if ($status !== 1) {
+      $this->logger->warning('Could not process entity @id from @queue, migration returned with status: @status.', [
+        '@id' => $entity,
+        '@queue' => $item['id'],
+        '@status' => $status,
+      ]);
+
+      throw new \Exception(sprintf(
+        'Could not process entity %s from %s, migration returned with status: %s.',
+        $entity,
+        $item['id'],
+        $status,
+      ));
+    }
+
+    $this->logger->info('Migrated item from @queue with data: @operation, @id.', [
+      '@queue' => $item['id'],
+      '@operation' => $operation,
+      '@id' => $entity,
+    ]);
+  }
+}
