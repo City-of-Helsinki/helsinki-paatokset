35380190534c6a616629053d8fc18c8f86a5caba
Jere Ljungberg <jere@mysteerio.fi>
2022-03-02 16:50:11 +0200

    PP-302: Add new processor + modify sector processor to enable decision maker data in index

--- public/modules/custom/paatokset_helsinki_kanava/src/Form/HelsinkiKanavaForm.php
+++ public/modules/custom/paatokset_helsinki_kanava/src/Form/HelsinkiKanavaForm.php
@@ -39,6 +39,20 @@ class HelsinkiKanavaForm extends ConfigFormBase {
       '#description' => 'Id for the city council policymaker.',
     ];
 
+    $form['city_hall_id'] = [
+      '#type' => 'textfield',
+      '#default_value' => $config->get('city_hall_id'),
+      '#title' => t('City Hall ID'),
+      '#description' => 'Id for the city hall policymaker',
+    ];
+
+    $form['trustee_organization_type_id'] = [
+      '#type' => 'textfield',
+      '#default_value' => $config->get('trustee_organization_type_id'),
+      '#title' => t('Trustee organization type id'),
+      '#description' => 'Id for the trustee type for policymakers',
+    ];
+
     $form['all_recordings_link'] = [
       '#type' => 'url',
       '#default_value' => $config->get('all_recordings_link'),
@@ -64,6 +78,8 @@ class HelsinkiKanavaForm extends ConfigFormBase {
 
     $this->config('paatokset_helsinki_kanava.settings')
       ->set('city_council_id', $form_state->getValue('city_council_id'))
+      ->set('city_hall_id', $form_state->getValue('city_hall_id'))
+      ->set('trustee_organization_type_id', $form_state->getValue('trustee_organization_type_id'))
       ->set('all_recordings_link', $form_state->getValue('all_recordings_link'))
       ->set('debug_mode', $form_state->getValue('debug_mode'))
       ->save();
