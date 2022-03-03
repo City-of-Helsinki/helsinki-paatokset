60d1c89beaee60c7d1fec98c52d0b0c6d552fa3b
Juho Lehmonen <juho.lehmonen@siili.com>
2022-03-03 15:25:16 +0200

    PP-305: Add cron shell scripts, edit deploy script.

---
+++ docker/openshift/crons/base.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Starting cron: $(date)"
+
+# You can add any additional cron "daemons" here:
+#
+# exec "/crons/some-command.sh" &
+#
+# Example cron (docker/openshift/crons/some-command.sh):
+# @code
+# #!/bin/bash
+# while true
+# do
+#   drush some-command
+#   sleep 600
+# done
+# @endcode
+
+exec "/crons/update-translations.sh" &
+exec "/crons/run-callback-queue.sh" &
+
+while true
+do
+  echo "Running cron: $(date)\n"
+  drush cron
+  # Sleep for 10 minutes.
+  sleep 600
+done
--- docker/openshift/entrypoints/90-deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /var/www/html/public && drush deploy
