4606efcd8dfa3379b98cfeb9368fef8e1d047833
Juho Lehmonen <juho.lehmonen@siili.com>
2022-03-07 15:33:40 +0200

    PP-305: Add single command for running all migration commands.

---
+++ tools/make/project/ahjo.mk
@@ -0,0 +1,10 @@
+PHONY += ahjo-migrations
+ahjo-migrations: # Run Ahjo migrations from local data.
+	$(call drush_on_${RUN_ON},ahjo-proxy:store-static-files)
+	$(call drush_on_${RUN_ON},migrate-import ahjo_cases:latest)
+	$(call drush_on_${RUN_ON},migrate-import ahjo_meetings:latest)
+	$(call drush_on_${RUN_ON},migrate-import ahjo_decisions:latest)
+	$(call drush_on_${RUN_ON},migrate-import ahjo_decisionmakers:all)
+	$(call drush_on_${RUN_ON},migrate-import ahjo_trustees:council)
+	$(call drush_on_${RUN_ON},ahjo-proxy:get-motions --localdata)
+	$(call drush_on_${RUN_ON},ahjo-proxy:update-decisions --update)
