c7f6cf41b4b10afc055c0b061711e6c83327192f
Juho Lehmonen <juho.lehmonen@siili.com>
2022-03-25 08:39:24 +0200

    PP-0: Reorder migration scripts for cron.

--- docker/openshift/crons/base.sh
+++ docker/openshift/crons/base.sh
@@ -18,8 +18,8 @@ echo "Starting cron: $(date)"
 
 exec "/crons/update-translations.sh" &
 exec "/crons/run-helsinki-integrations.sh" &
-exec "/crons/run-immediate-ahjo-integrations.sh" &
 exec "/crons/run-aggregated-ahjo-integrations.sh" &
+exec "/crons/run-immediate-ahjo-integrations.sh" &
 
 while true
 do
