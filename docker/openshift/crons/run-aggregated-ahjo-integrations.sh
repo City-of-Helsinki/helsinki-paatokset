8ac497fdec1e0c511dadf7862275f75d4882598b
Juho Lehmonen <juho.lehmonen@siili.com>
2022-03-25 08:39:24 +0200

    PP-0: Reorder migration scripts for cron.

--- docker/openshift/crons/run-aggregated-ahjo-integrations.sh
+++ docker/openshift/crons/run-aggregated-ahjo-integrations.sh
@@ -5,28 +5,27 @@ do
   # Sleep for 1 hour.
   sleep 3600
   echo "Aggregating data for meetings: $(date)"
-  drush ahjo-proxy:aggregate meetings --dataset=latest
+  drush ahjo-proxy:aggregate meetings --dataset=latest -v
   echo "Aggregating data for cases: $(date)"
-  drush ahjo-proxy:aggregate cases --dataset=latest
-  echo "Migrating data for cases: $(date)"
+  drush ahjo-proxy:aggregate cases --dataset=latest -v
   echo "Aggregating data for decisions: $(date)"
-  drush ahjo-proxy:aggregate decisions --dataset=latest
+  drush ahjo-proxy:aggregate decisions --dataset=latest -v
   echo "Aggregating council org data: $(date)"
-  drush ahjo-proxy:get-council-positionsoftrust
+  drush ahjo-proxy:get-council-positionsoftrust -v
   echo "Aggregating data for council members: $(date)"
-  drush ahjo-proxy:get-trustees positionsoftrust_council.json
+  drush ahjo-proxy:get-trustees positionsoftrust_council.json -v
   echo "Migrating data for meetings: $(date)"
+  drush migrate-reset-status ahjo_meetings:latest
   drush migrate-import ahjo_meetings:latest --update
   echo "Migrating data for cases: $(date)"
+  drush migrate-reset-status ahjo_cases:latest
   drush migrate-import ahjo_cases:latest --update
   echo "Migrating data for decisions: $(date)"
+  drush migrate-reset-status ahjo_decisions:latest
   drush migrate-import ahjo_decisions:latest --update
   echo "Migrating data for council members: $(date)"
+  drush migrate-reset-status ahjo_trustees:council
   drush migrate-import ahjo_trustees:council --update
-  echo "Generating motions from meeting data: $(date)"
-  drush ahjo-proxy:get-motions
-  echo "Updating decision and motion data: $(date)"
-  drush ahjo-proxy:update-decisions
   # Sleep for 23 hours.
   sleep 82800
 done
