41e422690b96348ee0eecc8eeb892285eefba255
Juho Lehmonen <juho.lehmonen@siili.com>
2022-06-13 10:07:26 +0300

    PP-379: PP-379: Add missing uniqueid and outdated document decision update scripts to cron jobs.

--- docker/openshift/crons/run-immediate-ahjo-integrations.sh
+++ docker/openshift/crons/run-immediate-ahjo-integrations.sh
@@ -8,6 +8,8 @@ do
   drush ahjo-proxy:get-motions
   echo "Updating decision and motion data: $(date)"
   drush ahjo-proxy:update-decisions
+  drush ahjo-proxy:update-decisions --logic=uniqueid --limit=100
+  drush ahjo-proxy:update-decisions --logic=outdated --limit=100
   # Sleep for 5 minutes.
   sleep 300
 done
