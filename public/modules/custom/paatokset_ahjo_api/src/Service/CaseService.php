207dccf9d93e2c35f8c2213e8504acc17a29d9bf
Juho Lehmonen <juho.lehmonen@siili.com>
2022-07-06 15:39:20 +0300

    PP-336: Better handling of decision attachments, including missing files and publicityclass info.

--- public/modules/custom/paatokset_ahjo_api/src/Service/CaseService.php
+++ public/modules/custom/paatokset_ahjo_api/src/Service/CaseService.php
@@ -795,12 +795,26 @@ class CaseService {
     $attachments = [];
     foreach ($this->selectedDecision->get('field_decision_attachments') as $field) {
       $data = json_decode($field->value, TRUE);
-      if (empty($data['FileURI']) || empty($data['Title'])) {
-        continue;
+
+      $publicity_class = NULL;
+      if (isset($data['PublicityClass'])) {
+        $publicity_class = $data['PublicityClass'];
       }
+
+      $file_url = NULL;
+      if (isset($data['FileURI'])) {
+        $file_url = $data['FileURI'];
+      }
+
+      $title = t('Attachment');
+      if (isset($data['Title'])) {
+        $title = $data['Title'];
+      }
+
       $attachments[] = [
-        'file_url' => $data['FileURI'],
-        'title' => $data['Title'],
+        'file_url' => $file_url,
+        'title' => $title,
+        'publicity_class' => $publicity_class,
       ];
     }
 
