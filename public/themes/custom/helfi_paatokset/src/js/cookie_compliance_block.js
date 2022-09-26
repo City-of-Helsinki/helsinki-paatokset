a468fe29a83f042a7772d0fb087e50050dbfab85
Juho Lehmonen <juho.lehmonen@siili.com>
2022-09-26 15:01:47 +0300

    PP-319: Cookie compliance JS override and style fixes.

---
+++ public/themes/custom/helfi_paatokset/src/js/cookie_compliance_block.js
@@ -0,0 +1,28 @@
+(($, Drupal) => {
+  Drupal.behaviors.paatoksetCookieComplianceBlock = {
+    attach: function attach() {
+      $(document).ready(() => {
+        const totalCategories = $(
+          "input.hds-checkbox__input",
+          ".eu-cookie-compliance-block-form"
+        );
+        const categories = Drupal.eu_cookie_compliance.getAcceptedCategories();
+        let selected = 0;
+        $.each(categories, (key, value) => {
+          $(`[data-drupal-selector="edit-categories-${value}"]`).prop(
+            "checked",
+            true
+          );
+          // eslint-disable-next-line no-plusplus
+          selected++;
+        });
+
+        if (selected < totalCategories.length) {
+          $('[data-drupal-selector="edit-accept-all"]')
+            .parent()
+            .removeClass("hidden");
+        }
+      });
+    },
+  };
+})(jQuery, Drupal);
