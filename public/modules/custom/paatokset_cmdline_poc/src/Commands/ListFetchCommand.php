8ee76082aabd8f05c06818be23047fdf007c692f
Juho Lehmonen <juho.lehmonen@siili.com>
2022-11-03 10:45:44 +0200

    PP-419: Comment all methods, fix PHPCS issues.

--- public/modules/custom/paatokset_cmdline_poc/src/Commands/ListFetchCommand.php
+++ public/modules/custom/paatokset_cmdline_poc/src/Commands/ListFetchCommand.php
@@ -11,20 +11,44 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Console\Output\OutputInterface;
+use GuzzleHttp\Psr7\Response;
 
-final class ListFetchCommand extends Command
-{
+/**
+ * Command for aggregating data outside of Drupal.
+ */
+final class ListFetchCommand extends Command {
+
+  /**
+   * Command name.
+   *
+   * @var string
+   */
   protected static $defaultName = 'app:list-fetch';
-  protected CONST BASE_URL = 'https://ahjo.hel.fi:9802/ahjorest/v1/';
-  protected CONST TOKEN_URL = 'https://nginx-paatokset-test.agw.arodevtest.hel.fi/fi/admin/ahjo-open-id/token';
 
-  public function __construct(private Client $client)
-  {
+  /**
+   * Ahjo API base URL.
+   */
+  protected const BASE_URL = 'https://ahjo.hel.fi:9802/ahjorest/v1/';
+
+  /**
+   * URL to fetch access token from.
+   */
+  protected const TOKEN_URL = 'https://nginx-paatokset-test.agw.arodevtest.hel.fi/fi/admin/ahjo-open-id/token';
+
+  /**
+   * Constructs list fetch command.
+   *
+   * @param \GuzzleHttp\Client $client
+   *   HTTP Client.
+   */
+  public function __construct(private Client $client) {
     parent::__construct();
   }
 
-  public function configure()
-  {
+  /**
+   * Configuration, adds arguments for command.
+   */
+  public function configure() {
     $this->addArgument(
       'endpoint',
       InputArgument::REQUIRED,
@@ -50,8 +74,18 @@ final class ListFetchCommand extends Command
     );
   }
 
-  protected function execute(InputInterface $input, OutputInterface $output): int
-  {
+  /**
+   * Executes command.
+   *
+   * @param \Symfony\Component\Console\Input\InputInterface $input
+   *   Input for command.
+   * @param \Symfony\Component\Console\Output\OutputInterface $output
+   *   Output for command.
+   *
+   * @return int
+   *   Exit code.
+   */
+  protected function execute(InputInterface $input, OutputInterface $output): int {
     $start_time = microtime(TRUE);
     $endpoint = $input->getArgument('endpoint');
     $start = $input->getArgument('start');
@@ -63,9 +97,6 @@ final class ListFetchCommand extends Command
       $output->writeln('No access token.');
       return 1;
     }
-
-    $output->writeln($access_token);
-    return 0;
 
     $list_url = $this->buildListUrl($endpoint, $start, $end);
     $output->writeln('Fetching list from: ' . $list_url);
@@ -109,7 +140,20 @@ final class ListFetchCommand extends Command
     return 0;
   }
 
-  protected function buildListUrl(string $endpoint, string $start, string $end) {
+  /**
+   * Builds API list URL.
+   *
+   * @param string $endpoint
+   *   Endpoint to get data from.
+   * @param string $start
+   *   Timestamp to start filtering from.
+   * @param string $end
+   *   Timestamp to end filtering on.
+   *
+   * @return string
+   *   List URL.
+   */
+  protected function buildListUrl(string $endpoint, string $start, string $end): string {
     $list_url = self::BASE_URL . $endpoint;
     $list_url .= '?count_limit=1000&size=1000';
     if ($endpoint === 'meetings') {
@@ -124,8 +168,20 @@ final class ListFetchCommand extends Command
     return $list_url;
   }
 
-
-  protected function doRequest(string $url, Output $output, string $access_token) {
+  /**
+   * Send HTTP GET request.
+   *
+   * @param string $url
+   *   URL to send the request to.
+   * @param \Symfony\Component\Console\Output\Output $output
+   *   Console output interface.
+   * @param string $access_token
+   *   Access token for API.
+   *
+   * @return \GuzzleHttp\Psr7\Response
+   *   HTTP Response.
+   */
+  protected function doRequest(string $url, Output $output, string $access_token): Response {
     $response = $this->client->request('GET', $url,
     [
       'http_errors' => FALSE,
@@ -140,13 +196,22 @@ final class ListFetchCommand extends Command
     return $response;
   }
 
-  protected function getAccessToken(string $api_key) {
+  /**
+   * Get API access token from Drupal.
+   *
+   * @param string $api_key
+   *   API key to authenticate with.
+   *
+   * @return string|null
+   *   Access token or NULL if it can't be fetched.
+   */
+  protected function getAccessToken(string $api_key): ?string {
     $response = $this->client->request('GET', self::TOKEN_URL,
     [
       'http_errors' => FALSE,
       'headers' => [
         'api-key' => $api_key,
-      ]
+      ],
     ]);
 
     if ($response->getStatusCode() !== 200) {
