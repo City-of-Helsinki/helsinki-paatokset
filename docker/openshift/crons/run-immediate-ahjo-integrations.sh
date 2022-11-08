119e0f15fe5c54e88d6180370cab54e9577b0990
Juho Lehmonen <juho.lehmonen@siili.com>
2022-11-08 11:54:20 +0200

    PP-419: Run immediate ahjo integrations with verbose flag.

--- docker/openshift/crons/run-immediate-ahjo-integrations.sh
+++ docker/openshift/crons/run-immediate-ahjo-integrations.sh
@@ -3,13 +3,13 @@
 while true
 do
   echo "Running Ahjo API callback queue: $(date)"
-  drush queue:run ahjo_api_subscriber_queue
+  drush queue:run ahjo_api_subscriber_queue -v
   echo "Generating motions from meeting data: $(date)"
-  drush ahjo-proxy:get-motions
+  drush ahjo-proxy:get-motions -v
   echo "Updating decision and motion data: $(date)"
-  drush ahjo-proxy:update-decisions
-  drush ahjo-proxy:update-decisions --logic=uniqueid --limit=100
-  drush ahjo-proxy:update-decisions --logic=outdated --limit=100
+  drush ahjo-proxy:update-decisions -v
+  drush ahjo-proxy:update-decisions --logic=uniqueid --limit=100 -v
+  drush ahjo-proxy:update-decisions --logic=outdated --limit=100 -v
   # Sleep for 5 minutes.
   sleep 300
 done
