8f3fd47fe88d18b4e1c028ac294295ca84b477b8
Juho Lehmonen <juho.lehmonen@siili.com>
2022-12-21 00:24:03 +0200

    PP-450: Reset meeting video migration in case it gets stuck. Comment out news migration.

--- docker/openshift/crons/run-helsinki-integrations.sh
+++ docker/openshift/crons/run-helsinki-integrations.sh
@@ -5,9 +5,10 @@ do
   # Sleep for 1 minute.
   sleep 360
   echo "Running Helsinki kanava migration: $(date)"
+  drush migrate-reset-status paatokset_meeting_videos
   drush migrate-import paatokset_meeting_videos --update
-  echo "Running news migration: $(date)"
-  drush migrate-import paatokset_news --update
+  #echo "Running news migration: $(date)"
+  #drush migrate-import paatokset_news --update
   # Sleep for 1 hour.
   sleep 3540
 done
