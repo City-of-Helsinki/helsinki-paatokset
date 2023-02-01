57bbffddd233397257c9f0fcbc944d3c4015b4e4
Juho Lehmonen <juho.lehmonen@siili.com>
2023-02-01 11:21:42 +0200

    Replace HDTB Matomo script with Päätökset Matomo script.

---
+++ public/themes/custom/helfi_paatokset/src/js/matomo.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line func-names
+(function ($, Drupal) {
+  function loadMatomoAnalytics() {
+
+    if (typeof Drupal.eu_cookie_compliance === 'undefined') {
+      return;
+    }
+
+    // Load Matomo only if statistics cookies are allowed.
+    if (Drupal.eu_cookie_compliance.hasAgreed('statistics')) {
+      // Matomo Tag Manager
+      // eslint-disable-next-line no-multi-assign
+      const _mtm = (window._mtm = window._mtm || []);
+      _mtm.push({
+        'mtm.startTime': new Date().getTime(),
+        event: 'mtm.Start',
+      });
+      const d = document;
+      const g = d.createElement('script');
+      const s = d.getElementsByTagName('script')[0];
+      g.type = 'text/javascript';
+      g.async = true;
+      g.src = '//webanalytics.digiaiiris.com/js/container_iNUwkZOx.js';
+
+      s.parentNode.insertBefore(g, s);
+    }
+  }
+
+  // Load when cookie settings are changed.
+  $(document).on('eu_cookie_compliance.changeStatus', loadMatomoAnalytics());
+
+  // Load on page load.
+  $(document).ready(loadMatomoAnalytics);
+})(jQuery, Drupal);
