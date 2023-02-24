f56edf7ba65a8c1672bc9dddf449bba0ab4e4315
Juho Lehmonen <juho.lehmonen@siili.com>
2023-02-24 10:42:25 +0200

    PP-453: Add series ID fetching and date update scripts for cron.

--- docker/openshift/crons/run-immediate-ahjo-integrations.sh
+++ docker/openshift/crons/run-immediate-ahjo-integrations.sh
@@ -12,12 +12,14 @@ do
   drush ahjo-proxy:get-motions -v
   echo "Updating decision and motion data: $(date)"
   drush ahjo-proxy:update-decisions -v
+  drush ahjo-proxy:update-decisions --logic=seriesid --limit=200 -v
   drush ahjo-proxy:update-decisions --logic=uniqueid --limit=100 -v
   drush ahjo-proxy:update-decisions --logic=outdated --limit=100 -v
   drush ahjo-proxy:update-decisions --logic=history --limit=100 -v
   drush ahjo-proxy:update-decisions --logic=case --limit=100 -v
   drush ahjo-proxy:update-decisions --logic=language --limit=100 -v
   drush ahjo-proxy:update-decision-attachments --limit=100 -v
+  drush ahjo-proxy:update-decision-dates --limit=100 -v
   # Sleep for 10 minutes.
   sleep 600
 done
