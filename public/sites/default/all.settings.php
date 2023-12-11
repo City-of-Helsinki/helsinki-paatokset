b283bc4a32af7e8e03f27e6a25a92740d229f606
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2023-12-11 08:40:50 +0200

    UHF-9308: Code style fixes

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -12,7 +12,7 @@ ini_set('default_socket_timeout', 240);
 if (getenv('ELASTIC_CONNECTOR_URL')) {
   $config['elasticsearch_connector.cluster.paatokset']['url'] = getenv('ELASTIC_CONNECTOR_URL');
 
-  if(getenv('ELASTIC_INTERNAL_USER') && getenv('ELASTIC_INTERNAL_PWD')) {
+  if (getenv('ELASTIC_INTERNAL_USER') && getenv('ELASTIC_INTERNAL_PWD')) {
     $config['elasticsearch_connector.cluster.paatokset']['options']['use_authentication'] = '1';
     $config['elasticsearch_connector.cluster.paatokset']['options']['authentication_type'] = 'Basic';
     $config['elasticsearch_connector.cluster.paatokset']['options']['username'] = getenv('ELASTIC_INTERNAL_USER');
