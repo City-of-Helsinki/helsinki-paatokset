e3f3c0adafff094bfb447e8d34083f14b18f761f
Juho Lehmonen <juho.lehmonen@siili.com>
2024-10-31 10:13:40 +0200

    UHF-10842: Do not try to refresh tokens during AHJO maintenance window.

--- docker/openshift/crons/refresh-ahjo-access-token.sh
+++ docker/openshift/crons/refresh-ahjo-access-token.sh
@@ -3,6 +3,15 @@
 while true
 do
   # Renew auth and refresh tokens every 60 minutes.
+
+  # Don't try to refresh tokens between 01:00 and 07:00 UTC+3.
+  # Ahjo might be offline for maintenance during the night.
+  currenttime=$(date +%H:%M)
+  if [[ "$currenttime" > "22:00" ]] || [[ "$currenttime" < "04:00" ]]; then
+    # Sleep for an hour until we're out of the maintenance window.
+    sleep 3600
+  else
+
   echo "Checking access token: $(date)"
   drush ahjo-proxy:check-auth-token refresh
   # Sleep for 60 minutes.
