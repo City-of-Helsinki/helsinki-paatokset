212bcddd7160b22d8e44f687dc9c955b7f0c3c16
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2025-01-30 08:13:51 +0200

    UHF-9030: Add test for datapumppu statement cache invalidation

--- public/modules/custom/paatokset_datapumppu/src/Entity/Statement.php
+++ public/modules/custom/paatokset_datapumppu/src/Entity/Statement.php
@@ -6,7 +6,6 @@ namespace Drupal\paatokset_datapumppu\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\EntityTypeInterface;
-use Drupal\Core\Entity\RevisionLogEntityTrait;
 use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 
@@ -61,7 +60,12 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
  */
 final class Statement extends ContentEntityBase {
 
-  use RevisionLogEntityTrait;
+  /**
+   * Get speaker id.
+   */
+  public function getSpeakerId(): ?string {
+    return $this->get('speaker')->target_id;
+  }
 
   /**
    * {@inheritdoc}
