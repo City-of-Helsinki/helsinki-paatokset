3d0206f590a4a7e0fd1fe05a62ba06f1cfdeac4b
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2025-01-30 08:15:11 +0200

    UHF-9030: Remove verbose output, use --reset-threshold flag

--- docker/openshift/crons/run-datapumppu-integrations.sh
+++ docker/openshift/crons/run-datapumppu-integrations.sh
@@ -6,11 +6,9 @@ sleep 100
 while true
 do
   echo "Running Datapumppu migration: $(date)"
-  drush migrate:import datapumppu_statements -v --no-progress
+  # Interval 6 hours, reset threshold 10 minutes.
+  drush migrate:import datapumppu_statements --interval 21600 --reset-threshold 600 --no-progress
 
   # Sleep for 24 hours.
   sleep 86400
-
-  echo "Reset datapumppu migration"
-  drush migrate-reset-status datapumppu_statements 2>/dev/null
 done
