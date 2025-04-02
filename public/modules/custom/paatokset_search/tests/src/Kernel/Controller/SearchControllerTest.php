a2a00d3f5764922a8508548dd2894b497e3a430d
Toni Sinisalo <toni.sinisalo@digitalist.com>
2025-04-02 17:15:06 +0300

    UHF-11527: Updating paatokset-search version & adding tests for new code.

---
+++ public/modules/custom/paatokset_search/tests/src/Kernel/Controller/SearchControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\paatokset_search\Kernel\Controller;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\paatokset_search\Controller\SearchController;
+
+/**
+ * Tests search controller.
+ *
+ * @coversDefaultClass \Drupal\paatokset_search\Controller\SearchController
+ */
+class SearchControllerTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'paatokset_search',
+  ];
+
+  /**
+   * Tests block render.
+   *
+   * @covers ::decisions
+   */
+  public function testDecisions(): void {
+    $controller = SearchController::create($this->container);
+    $build = $controller->decisions();
+
+    $this->assertNotEmpty($build);
+  }
+
+}
---
+++ public/modules/custom/paatokset_search/tests/src/Kernel/SearchManagerTest.php
@@ -0,0 +1,159 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\paatokset_search\Kernel;
+
+use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
+use Drupal\paatokset_search\SearchManager;
+use Drupal\Tests\node\Traits\NodeCreationTrait;
+use Drupal\node\Entity\NodeType;
+use Drupal\user\RoleInterface;
+
+/**
+ * Tests search manager.
+ *
+ * @coversDefaultClass \Drupal\paatokset_search\SearchManager
+ */
+class SearchManagerTest extends EntityKernelTestBase {
+
+  use NodeCreationTrait {
+    createNode as drupalCreateNode;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'paatokset_search',
+    'node',
+    'user',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $configSchemaCheckerExclusions = [
+    'elastic_proxy.settings',
+    'paatokset_search.settings',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // $this->strictConfigSchema = FALSE;
+    $this->installConfig(['system']);
+    $this->installSchema('node', ['node_access']);
+
+    // Clear permissions for authenticated users.
+    $this->config('user.role.' . RoleInterface::AUTHENTICATED_ID)
+      ->set('permissions', [])
+      ->save();
+    // Create user 1 who has special permissions.
+    $this->drupalCreateUser();
+
+    $user = $this->drupalCreateUser([
+      'access content',
+    ]);
+    $this->container->get('current_user')->setAccount($user);
+
+    NodeType::create([
+      'name' => $this->randomMachineName(),
+      'type' => 'page',
+    ]);
+  }
+
+  /**
+   * Tests search manager build with defaults.
+   *
+   * @covers ::build
+   */
+  public function testBuildWithDefaults(): void {
+    $this->setConfiguration([
+      'elastic_proxy_url' => 'https://example.com',
+    ], [
+      'sentry_dsn_react' => 'https://sentry.example.com',
+    ]);
+    $manager = $this->container->get(SearchManager::class);
+
+    $build = $manager->build('decisions');
+
+    $this->assertContains('paatokset_search/paatokset-search', $build['#attached']['library']);
+    $this->assertEquals('https://sentry.example.com', $build['#attached']['drupalSettings']['paatokset_react_search']['sentry_dsn_react']);
+    $this->assertEquals('decisions', $build['#attributes']['data-type']);
+    $this->assertEquals('https://example.com', $build['#attributes']['data-url']);
+    $this->assertEmpty($build['#attributes']['data-operator-guide-url']);
+  }
+
+  /**
+   * Tests search manager build with operator guide.
+   *
+   * @covers ::build
+   */
+  public function testBuildWithOperatorGuide(): void {
+    $node = $this->drupalCreateNode([
+      'type' => 'page',
+      'title' => 'Test node',
+    ]);
+    $node->save();
+
+    $this->setConfiguration([], [
+      'operator_guide_node_id' => $node->id(),
+    ]);
+    $manager = $this->container->get(SearchManager::class);
+
+    $build = $manager->build('decisions');
+
+    $this->assertEquals('/node/' . $node->id(), $build['#attributes']['data-operator-guide-url']);
+  }
+
+  /**
+   * Tests search manager build with unpublished operator guide.
+   *
+   * @covers ::build
+   */
+  public function testBuildWithUnpublishedOperatorGuide(): void {
+    $node = $this->drupalCreateNode([
+      'type' => 'page',
+      'title' => 'Test node',
+      'uid' => '1',
+      'status' => 0,
+    ]);
+    $node->save();
+
+    $this->setConfiguration([], [
+      'operator_guide_node_id' => $node->id(),
+    ]);
+    $manager = $this->container->get(SearchManager::class);
+
+    $build = $manager->build('decisions');
+
+    $this->assertEmpty($build['#attributes']['data-operator-guide-url']);
+  }
+
+  /**
+   * Helper function to set configuration.
+   *
+   * @param array $elastic_proxy
+   *   The elastic proxy configuration.
+   * @param array $paatokset_search
+   *   The paatokset search configuration.
+   */
+  private function setConfiguration(array $elastic_proxy = [], array $paatokset_search = []): void {
+    $elastic_proxy_config = $this->config('elastic_proxy.settings');
+    foreach ($elastic_proxy as $key => $value) {
+      $elastic_proxy_config->set($key, $value);
+    }
+    $elastic_proxy_config->save();
+
+    $paatokset_search_config = $this->config('paatokset_search.settings');
+    foreach ($paatokset_search as $key => $value) {
+      $paatokset_search_config->set($key, $value);
+    }
+    $paatokset_search_config->save();
+  }
+
+}
