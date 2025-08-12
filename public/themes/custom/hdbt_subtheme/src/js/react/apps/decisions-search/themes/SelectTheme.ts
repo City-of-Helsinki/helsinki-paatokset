23de6d0f377e8b1fca5abe95e07356258cb3476e
Jere Ljungberg <jere@mysteerio.fi>
2025-08-12 14:09:43 +0300

    UHF-11678: Remove pre-existing styles in select theme

--- public/themes/custom/hdbt_subtheme/src/js/react/apps/decisions-search/themes/SelectTheme.ts
+++ public/themes/custom/hdbt_subtheme/src/js/react/apps/decisions-search/themes/SelectTheme.ts
@@ -1,15 +1,12 @@
 export const SelectTheme = {
-  '--checkbox-background-hover': 'var(--hdbt-color-black)',
   '--checkbox-background-selected': 'var(--hdbt-color-black)',
-  '--checkbox-border-color-selected-focus': 'var(--hdbt-color-black)',
-  '--checkbox-border-color-selected-hover': 'var(--hdbt-color-black)',
+  '--checkbox-background-hover': 'var(--hdbt-color-black)',
   '--checkbox-border-color-selected': 'var(--hdbt-color-black)',
-  '--checkbox-icon-color-selected': 'var(--color-white)',
+  '--checkbox-border-color-selected-hover': 'var(--hdbt-color-black)',
+  '--checkbox-border-color-selected-focus': 'var(--hdbt-color-black)',
   '--focus-outline-color': 'var(--hdbt-color-black)',
-  '--menu-item-background-color-hover': 'var(--color-black-5)',
-  '--menu-item-background-color-selected-hover': 'var(--color-black-5)',
-  '--menu-item-background-color-selected': 'var(--color-white)',
   '--menu-item-border-color-focus': 'var(--hdbt-color-black)',
-  '--menu-item-color-hover': 'var(--color-white)',
-  '--menu-item-color-selected-hover': 'var(--color-white)',
+  '--menu-item-background-color-hover': 'var(--color-black-5)',
+  '--menu-item-background-color-selected': 'var(--color-white)',
+  '--menu-item-background-color-selected-hover': 'var(--color-black-5)'
 };
