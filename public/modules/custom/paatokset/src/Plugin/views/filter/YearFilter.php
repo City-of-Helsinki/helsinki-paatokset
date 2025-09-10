4cb43c0d3d18dda4c8224711699ef858f1ac27e1
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2025-09-10 18:24:29 +0300

    UHF-11192: Replace all_articles block plugin with a view

---
+++ public/modules/custom/paatokset/src/Plugin/views/filter/YearFilter.php
@@ -0,0 +1,121 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\paatokset\Plugin\views\filter;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\views\Attribute\ViewsFilter;
+use Drupal\views\Plugin\views\filter\FilterPluginBase;
+use Drupal\views\Plugin\views\query\Sql;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Filters results by year.
+ */
+#[ViewsFilter("paatokset_year")]
+final class YearFilter extends FilterPluginBase {
+
+  /**
+   * Database connection.
+   */
+  private Connection $connection;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): static {
+    $instance = new self($configuration, $plugin_id, $plugin_definition);
+    $instance->connection = $container->get(Connection::class);
+    return $instance;
+  }
+
+  /**
+   * Provide field for selecting the year.
+   */
+  protected function valueForm(&$form, FormStateInterface $form_state): void {
+    $options = $this->getOptions();
+
+    $form['value'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Year'),
+      '#placeholder' => $this->t('All years'),
+      '#options' => $options,
+      '#default_value' => $this->value,
+    ];
+
+    $cache = new BubbleableMetadata();
+
+    $entityType = $this->options['entity_type'];
+    $allFilters = $this->displayHandler->getOption('filters');
+
+    // Select element options must be invalidated when new
+    // entity is inserted. Add <entity type ID>_list:<bundle>
+    // cache tags (or <entity type ID>_list).
+    if (isset($allFilters['type'])) {
+      $cache->addCacheTags(array_map(fn ($type) => $entityType . "_list:$type", array_keys($allFilters['type']['value'])));
+    }
+    else {
+      $cache->addCacheTags([$entityType . '_list']);
+    }
+
+    $cache->applyTo($form);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function query() {
+    [$year] = $this->value;
+
+    if (empty($year)) {
+      return;
+    }
+
+    $startOfYear = mktime(0, 0, 0, 1, 1, (int) $year);
+    $endOfYear = mktime(23, 59, 59, 12, 31, (int) $year);
+
+    $this->ensureMyTable();
+    assert($this->query instanceof Sql);
+    $this->query->addWhere(
+      $this->options['group'],
+      "$this->tableAlias.$this->realField",
+      [$startOfYear, $endOfYear],
+      'BETWEEN'
+    );
+  }
+
+  /**
+   * Get options for year select.
+   */
+  private function getOptions(): array {
+    $allFilters = $this->displayHandler->getOption('filters');
+
+    $query = $this->connection
+      ->select($this->table, 'tbl')
+      ->isNotNull("tbl.$this->realField")
+      ->condition("tbl.status", 1)
+      ->distinct();
+
+    // Query only configured entity
+    // types if type filter is set.
+    if (isset($allFilters['type'])) {
+      [
+        'field' => $field,
+        'value' => $bundles,
+      ] = $allFilters['type'];
+
+      $query->condition("tbl.$field", array_keys($bundles), 'IN');
+    }
+
+    $query->addExpression("FROM_UNIXTIME(tbl.$this->realField, '%Y')", 'year');
+    $query->orderBy('year', 'DESC');
+
+    return $query
+      ->execute()
+      ->fetchAllKeyed(0, 0);
+  }
+
+}
