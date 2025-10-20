c48b4ded716cc32fce16fefdc64f3b1bfd914387
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2025-10-20 09:43:51 +0300

    Simplify test

--- public/modules/custom/paatokset_ahjo_api/tests/src/Kernel/AhjoOpenId/SettingsTest.php
+++ public/modules/custom/paatokset_ahjo_api/tests/src/Kernel/AhjoOpenId/SettingsTest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\paatokset_ahjo_api\AhjoOpenId\Kernel;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\paatokset_ahjo_api\AhjoOpenId\Settings;
 use Drupal\paatokset_ahjo_api\AhjoOpenId\SettingsFactory;
@@ -42,10 +40,8 @@ class SettingsTest extends KernelTestBase {
       ])
       ->save();
 
-    $sut = new SettingsFactory(
-      $this->container->get(ConfigFactoryInterface::class),
-      $this->container->get(LanguageManagerInterface::class)
-    );
+    $sut = $this->container->get('paatokset_ahjo_openid.settings_factory');
+    $this->assertInstanceOf(SettingsFactory::class, $sut);
     $settings = $sut->create();
     $this->assertInstanceOf(Settings::class, $settings);
     foreach ($expectedValues as $key => $value) {
