d3671a788f049756c4ce627270da7fac8769a89f
tuutti <tuutti@iki.fi>
2025-10-22 11:43:44 +0300

    UHF-12143: Lupapiste debug data plugin

---
+++ public/modules/custom/paatokset/src/Plugin/DebugDataItem/Lupapiste.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\paatokset\Plugin\DebugDataItem;
+
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\helfi_api_base\Attribute\DebugDataItem;
+use Drupal\helfi_api_base\Debug\SupportsValidityChecksInterface;
+use Drupal\helfi_api_base\DebugDataItemPluginBase;
+use Drupal\paatokset\Lupapiste\ItemsImporter;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Debug data plugin for Lupapiste RSS service.
+ *
+ * This is used to ensure Drupal can connect to the Lupapiste service.
+ */
+#[DebugDataItem(
+  id: 'lupapiste',
+  title: new TranslatableMarkup('Lupapiste'),
+)]
+final class Lupapiste extends DebugDataItemPluginBase implements ContainerFactoryPluginInterface, SupportsValidityChecksInterface {
+
+  /**
+   * The Lupapiste data importer.
+   *
+   * @var \Drupal\paatokset\Lupapiste\ItemsImporter
+   */
+  private readonly ItemsImporter $importer;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+  ) : self {
+    $instance = new self($configuration, $plugin_id, $plugin_definition);
+    $instance->importer = $container->get(ItemsImporter::class);
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function check(): bool {
+    $data = $this->importer->fetch('fi');
+
+    return !empty($data['items']);
+  }
+
+}
