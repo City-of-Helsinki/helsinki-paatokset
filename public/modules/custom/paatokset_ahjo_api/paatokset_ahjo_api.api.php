0d4e15ff3aecf0e937d8b79b07be49cd3cb44d02
Anna Lonka <anna.lonka@druid.fi>
2025-11-26 15:59:10 +0200

    UHF-12392: Added allowed values function

---
+++ public/modules/custom/paatokset_ahjo_api/paatokset_ahjo_api.api.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Module paatokset_ahjo_api hooks.
+ */
+
+declare(strict_types=1);
+
+/**
+ * Alter the organization color selection array.
+ *
+ * @param array $designs
+ *   The designs for the content cards.
+ */
+function hook_paatokset_ahjo_api_organization_color_alter(array &$colors) {
+
+}
