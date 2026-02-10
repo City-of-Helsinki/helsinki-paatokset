ae2aff44564e27f34bbd4f0a4f905e9fcc741336
Jere Ljungberg <jere@mysteerio.fi>
2026-02-10 10:48:01 +0200

    UHF-12379: Remove old search app

--- public/themes/custom/hdbt_subtheme/src/js/react/apps/decisions-search-old/global.d.ts
+++
@@ -1,1 +0,0 @@
-// File for type declarations
--- public/themes/custom/hdbt_subtheme/src/js/react/apps/decisions-search/index.tsx
+++ public/themes/custom/hdbt_subtheme/src/js/react/apps/decisions-search/index.tsx
@@ -3,7 +3,7 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { GhostList } from '@/react/common/GhostList';
 import ResultsError from '@/react/common/ResultsError';
-import initSentry from '../decisions-search-old/common/Sentry';
+import initSentry from '@/react/common/helpers/Sentry';
 import { DecisionsContainer } from './modules/decisions/DecisionsContainer';
 import { PolicymakerContainer } from './modules/policymakers/PolicymakerContainer';
 
