42d451bd0714de5a3fe1b051041335ee7b42af90
Anna Lonka <anna.lonka@druid.fi>
2026-03-10 09:49:29 +0200

    UHF-12835: Changed how language manager is used

--- public/modules/custom/paatokset_ahjo_api/src/Policymakers/BrowseBreadcrumbBuilder.php
+++ public/modules/custom/paatokset_ahjo_api/src/Policymakers/BrowseBreadcrumbBuilder.php
@@ -6,6 +6,7 @@ namespace Drupal\paatokset_ahjo_api\Policymakers;
 
 use Drupal\Core\Breadcrumb\Breadcrumb;
 use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Link;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
@@ -23,6 +24,17 @@ class BrowseBreadcrumbBuilder implements BreadcrumbBuilderInterface {
   use StringTranslationTrait;
 
   /**
+   * Constructs BrowseBreadcrumbBuilder.
+   *
+   * @param \Drupal\Core\Language\LanguageManagerInterface $languageManager
+   *   The language manager.
+   */
+  public function __construct(
+    private readonly LanguageManagerInterface $languageManager,
+  ) {
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function applies(RouteMatchInterface $route_match): bool {
@@ -37,7 +49,7 @@ class BrowseBreadcrumbBuilder implements BreadcrumbBuilderInterface {
 
     $breadcrumb->addLink(Link::createFromRoute($this->t('Home'), '<front>'));
 
-    $langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    $langcode = $this->languageManager->getCurrentLanguage()->getId();
 
     $route = match($langcode) {
       'fi' => '/fi/paatoksenteko',
