0d3bf29ba0d767c049936e43e37e9fbec437c5c4
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-03-27 16:28:53 +0200

    UHF-12831: Move command to symfony format

--- public/modules/custom/paatokset_ahjo_api/src/Drush/Commands/AhjoTokenCommands.php
+++ public/modules/custom/paatokset_ahjo_api/src/Drush/Commands/AhjoTokenCommands.php
@@ -4,21 +4,25 @@ declare(strict_types=1);
 
 namespace Drupal\paatokset_ahjo_api\Drush\Commands;
 
-use Consolidation\AnnotatedCommand\Attributes\Command;
 use Drupal\Core\Utility\Error;
 use Drupal\paatokset_ahjo_api\AhjoOpenId\AhjoOpenId;
 use Drupal\paatokset_ahjo_api\AhjoOpenId\AhjoOpenIdException;
 use Drush\Commands\AutowireTrait;
-use Drush\Commands\DrushCommands;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 /**
  * Ahjo token commands.
- *
- * @package Drupal\paatokset_ahjo_api\Drush\Commands
  */
-final class AhjoTokenCommands extends DrushCommands {
+#[AsCommand(
+  name: 'ahjo-api:refresh-token',
+  description: 'Refresh AHJO auth token.',
+)]
+final class AhjoTokenCommands extends Command {
 
   use AutowireTrait;
 
@@ -30,22 +34,18 @@ final class AhjoTokenCommands extends DrushCommands {
     parent::__construct();
   }
 
-  /**
-   * Refresh AHJO auth token.
-   */
-  #[Command(name: 'ahjo-api:refresh-token')]
-  public function refreshAuthToken(): int {
+  protected function execute(InputInterface $input, OutputInterface $output): int {
     try {
       $this->ahjoOpenId->refreshAuthToken();
 
-      return DrushCommands::EXIT_SUCCESS;
+      return self::SUCCESS;
     }
     catch (AhjoOpenIdException $e) {
       // logException does not work with Drush logger.
       Error::logException($this->drupalLogger, $e);
     }
 
-    return DrushCommands::EXIT_FAILURE;
+    return self::FAILURE;
   }
 
 }
