075c3ea0fcf882b146f8a76d9360cd68d7825ecd
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-04-27 06:56:08 +0300

    UHF-12964: Remove react select app

--- public/themes/custom/hdbt_subtheme/src/js/react/apps/select/types/drupalSettings.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Drupal {
-  const behaviors: { helfiSelect: { attach: (context: HTMLElement) => void } };
-}
-
-declare namespace drupalSettings {
-  const path: { currentLanguage: 'fi' | 'en' | 'sv' };
-}
-
-type HelfiSelectSettings = { value?: string; empty_option?: string; options: { [key: string]: string } };
