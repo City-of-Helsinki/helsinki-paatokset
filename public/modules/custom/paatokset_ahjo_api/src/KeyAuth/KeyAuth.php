98271fb292e1049ed926477458e98639f4ac1319
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-06-11 13:18:30 +0300

    Read ahjo token from correct header

--- public/modules/custom/paatokset_ahjo_api/src/KeyAuth/KeyAuth.php
+++ public/modules/custom/paatokset_ahjo_api/src/KeyAuth/KeyAuth.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\paatokset_ahjo_api\KeyAuth;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Logger\LoggerChannelInterface;
 use Drupal\key_auth\KeyAuth as KeyAuthBase;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -19,29 +15,13 @@ use Symfony\Component\HttpFoundation\Request;
  */
 final class KeyAuth extends KeyAuthBase {
 
-  public function __construct(
-    ConfigFactoryInterface $config_factory,
-    EntityTypeManagerInterface $entity_type_manager,
-    #[Autowire(service: 'logger.channel.paatokset_ahjo_api')]
-    private readonly LoggerChannelInterface $logger,
-  ) {
-    parent::__construct($config_factory, $entity_type_manager);
-  }
-
   /**
    * {@inheritdoc}
    */
   public function getKey(Request $request) : false|string {
-    // AHJO sends requests using an 'Authorization: Token <token>' header.
-    $authorization = $request->headers->get('Authorization', '');
-    if (preg_match('/^Token (.+)$/i', $authorization, $matches)) {
-      return $matches[1] ?: FALSE;
-    }
-    // Fall back to a plain 'Token: <token>' header.
-    if ($request->headers->has('Token')) {
-      // If these log entries don't appear, the fallback can be removed.
-      $this->logger->info("Used 'Token' header fallback for key authentication.");
-      return $request->headers->get('Token') ?: FALSE;
+    // AHJO sends the key in 'Authorization' header.
+    if ($authorization = $request->headers->get('Authorization')) {
+      return $authorization;
     }
     return parent::getKey($request);
   }
