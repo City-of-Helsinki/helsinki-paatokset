c736a81978449fb806821985cfd8246fbeb93cb8
Toni Sinisalo <toni.sinisalo@digitalistgroup.com>
2026-07-27 14:51:11 +0300

    UHF-13250: Updating db-replace cleanup step to disable maintenance mode before launching the lengthy drush command. Also adding a timeout option to allow the cleanup to run max 5 hours.

--- docker/openshift/hooks/db-replace/99-clean.sh
+++ docker/openshift/hooks/db-replace/99-clean.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-drush paatokset:decisions:delete
+# Clear maintenance mode to allow access to the site while cleaning up
+# the database.
+drush state:set system.maintenance_mode 0 --input-format=integer
+
+# Delete old decisions. Allow it to run for max 5 hours.
+drush paatokset:decisions:delete --timeout=18000
