42b33e2d8c900c6197edd837ca724cdf247f9243
Jere Ljungberg <jere@mysteerio.fi>
2026-07-29 16:10:54 +0300

    UHF-11117: Fix issues with React 19 update

--- public/themes/custom/hdbt_subtheme/src/js/react/apps/allu-decisions-search/index.tsx
+++ public/themes/custom/hdbt_subtheme/src/js/react/apps/allu-decisions-search/index.tsx
@@ -1,6 +1,6 @@
 import { ErrorBoundary } from '@sentry/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import ResultsError from '@/react/common/ResultsError';
 import { SearchContainer } from './containers/SearchContainer';
 
@@ -8,12 +8,11 @@ const rootSelector = 'allu-decisions-search';
 const rootElement = document.getElementById(rootSelector);
 
 if (rootElement) {
-  ReactDOM.render(
+  createRoot(rootElement).render(
     <React.StrictMode>
       <ErrorBoundary fallback={<ResultsError error='Allu decisions search crashed' />}>
         <SearchContainer />
       </ErrorBoundary>
     </React.StrictMode>,
-    rootElement,
   );
 }
